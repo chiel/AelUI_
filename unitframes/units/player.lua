6b67e475a78018ffda75412d5b9a8294ebf0f926
Chiel Kunkels <chiel@kunkels.me>
2020-08-09 20:43:46 +0200

    Leave space for brewmaster stagger bar WA

--- unitframes/units/player.lua
+++ unitframes/units/player.lua
@@ -1,5 +1,31 @@
 local addon = select(2, ...)
 
+local updateClassResources = function(frame, unit)
+	local playerClass = select(2, UnitClass('player'))
+	local playerSpec = addon.utils.GetSpecName()
+	local hasClassResources = false
+
+	if playerClass == 'DEATHKNIGHT' then
+		local runeFrame = addon.elements.Runes(frame, unit)
+		runeFrame:SetPoint('TOP', UIParent, 'CENTER', 0, -310)
+		runeFrame:SetSize(298, 5)
+		hasClassResources = true
+
+	elseif playerClass == 'MONK' then
+		if playerSpec == 'BREWMASTER' then
+			hasClassResources = true
+		end
+	end
+
+	if hasClassResources then
+		frame.Power:SetPoint('TOP', UIParent, 'CENTER', 0, -316)
+		frame.Power:SetHeight(4)
+	else
+		frame.Power:SetPoint('TOP', UIParent, 'CENTER', 0, -310)
+		frame.Power:SetHeight(10)
+	end
+end
+
 addon.units.player = {
 	spawn = function(self)
 		local frame = self:Spawn('player')
@@ -14,28 +40,22 @@ addon.units.player = {
 		frame.Health:SetAllPoints()
 
 		addon.elements.Power(frame, unit)
-		frame.Power:SetPoint('TOP', UIParent, 'CENTER', 0, -310)
-		frame.Power:SetSize(298, 10)
+		frame.Power:SetWidth(298)
 
 		addon.elements.Castbar(frame, unit)
 		frame.Castbar:SetPoint('TOP', UIParent, 'CENTER', 0, -326)
 		frame.Castbar:SetWidth(298)
 
-		local playerClass = select(2, UnitClass('player'))
-		local playerSpec = addon.utils.GetSpecName()
-		local hasClassResources = false
+		updateClassResources(frame, unit)
 
-		if playerClass == 'DEATHKNIGHT' then
-			local runeFrame = addon.elements.Runes(frame, unit)
-			runeFrame:SetPoint('TOP', UIParent, 'CENTER', 0, -310)
-			runeFrame:SetSize(298, 5)
-
-			hasClassResources = true
-		end
-
-		if hasClassResources then
-			frame.Power:SetPoint('TOP', UIParent, 'CENTER', 0, -316)
-			frame.Power:SetHeight(4)
-		end
+		local f = CreateFrame('Frame', nil, UIParent)
+		f:RegisterEvent('PLAYER_SPECIALIZATION_CHANGED')
+		f:SetScript('OnEvent', function(self, event, u)
+			if event == 'PLAYER_SPECIALIZATION_CHANGED' then
+				if u == 'player' then
+					updateClassResources(frame, unit)
+				end
+			end
+		end)
 	end,
 }
