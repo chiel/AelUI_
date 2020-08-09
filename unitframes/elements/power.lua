fe9e3a969edd6bb12b07138b1a858d8734e31ff2
Chiel Kunkels <chiel@kunkels.me>
2020-08-09 20:40:40 +0200

    Update Power variable naming

--- unitframes/elements/power.lua
+++ unitframes/elements/power.lua
@@ -1,13 +1,13 @@
 print("AelUI: unitframes/elements/power.lua")
 local addon = select(2, ...)
 
-addon.elements.Power = function(self, unit)
-	local Power = CreateFrame('StatusBar', nil, self)
+addon.elements.Power = function(frame, unit)
+	local Power = CreateFrame('StatusBar', nil, frame)
 	Power:SetStatusBarTexture(addon.media.texture)
 	Power.frequentUpdates = true
 	Power.colorPower = true
 
 	addon.elements.CreateBg(Power)
 
-	self.Power = Power
+	frame.Power = Power
 end
