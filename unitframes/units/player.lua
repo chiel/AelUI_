12abe51cace1fc7eb1cac9a10c24ca8ba7ab629b
Chiel Kunkels <chiel@kunkels.me>
2020-08-09 19:23:45 +0200

    Adjust resource/castbar positioning

--- unitframes/units/player.lua
+++ unitframes/units/player.lua
@@ -4,7 +4,7 @@ addon.units.player = {
 	spawn = function(self)
 		local frame = self:Spawn('player')
 		frame:SetSize(300, 30)
-		frame:SetPoint('TOPRIGHT', UIParent, 'CENTER', -192, -300)
+		frame:SetPoint('TOPRIGHT', UIParent, 'CENTER', -191, -300)
 	end,
 
 	style = function(frame, unit)
@@ -14,12 +14,11 @@ addon.units.player = {
 		frame.Health:SetAllPoints()
 
 		addon.elements.Power(frame, unit)
-		frame.Power:SetPoint('TOP', UIParent, 'CENTER', 0, -300)
-		frame.Power:SetSize(300, 12)
+		frame.Power:SetPoint('TOP', UIParent, 'CENTER', 0, -310)
+		frame.Power:SetSize(298, 10)
 
 		addon.elements.Castbar(frame, unit)
-		frame.Castbar:SetPoint('TOPLEFT', frame.Power, 'BOTTOMLEFT', 0, -6)
-		frame.Castbar:SetPoint('TOPRIGHT', frame.Power, 'BOTTOMRIGHT', 0, -6)
-		frame.Castbar:SetHeight(12)
+		frame.Castbar:SetPoint('TOP', UIParent, 'CENTER', 0, -326)
+		frame.Castbar:SetWidth(298)
 	end,
 }
