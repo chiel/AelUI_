1284bfb411035d2fd67ca4c4b3bd3f12faa5937d
Chiel Kunkels <chiel@kunkels.me>
2020-08-10 14:22:41 +0200

    Add Hex helper function

---
+++ utils/Hex.lua
@@ -0,0 +1,14 @@
+print('AelUI: utils/GetSpecName.lua')
+local addon = select(2, ...)
+
+addon.utils.Hex = function(r, g, b)
+	if(type(r) == 'table') then
+		if(r.r) then
+			r, g, b = r.r, r.g, r.b
+		else
+			r, g, b = unpack(r)
+		end
+	end
+
+	return string.format('|cff%02x%02x%02x', r * 255, g * 255, b * 255)
+end
