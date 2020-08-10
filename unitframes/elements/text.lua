75db410d1b6b97de78880e5e08a5a8a7cf9548a5
Chiel Kunkels <chiel@kunkels.me>
2020-08-10 16:12:56 +0200

    Add Text element

---
+++ unitframes/elements/text.lua
@@ -0,0 +1,17 @@
+print("AelUI: unitframes/elements/text.lua")
+local addon = select(2, ...)
+
+addon.elements.Text = function(parent, options)
+	local options = options or {}
+	options.pixel = options.pixel or false
+	options.size = options.size or 20
+
+	local font = options.pixel and addon.media.fontPixel or addon.media.font
+	local flags = options.pixel and 'MONOCHROME,OUTLINE' or 'OUTLINE'
+
+	local Text = parent:CreateFontString(nil, 'OVERLAY', 'GameFontNormal')
+	Text:SetFont(font, options.size, flags)
+	Text:SetTextColor(1, 1, 1)
+
+	return Text
+end
