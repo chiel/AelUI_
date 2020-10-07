29b3857cab016d3669666b8d8d756e3a70c93fbd
Chiel Kunkels <chiel@kunkels.me>
2020-10-08 00:57:03 +0200

    utils: add method to determine if unit is healer

---
+++ utils/UnitIsHealer.lua
@@ -0,0 +1,37 @@
+local addon = select(2, ...)
+
+local healerClasses = {
+	DRUID = true,
+	MONK = true,
+	PALADIN = true,
+	PRIEST = true,
+	SHAMAN = true,
+}
+
+function addon.utils.UnitIsHealer(unit)
+	if not UnitIsConnected(unit) or UnitHasVehicleUI(unit) then
+		return false
+	end
+
+	local _, class = UnitClass(unit)
+	if not healerClasses[class] then
+		return false
+	end
+
+	local hasMana = UnitPowerType(unit) == Enum.PowerType.Mana
+	if not hasMana then
+		return false
+	end
+
+	local role = UnitGroupRolesAssigned(unit)
+	if role == 'HEALER' then
+		return true
+	end
+
+	local maxPower = UnitPowerMax(unit, Enum.PowerType.Mana)
+	if maxPower >= 100000 then
+		return true
+	end
+
+	return false
+end
