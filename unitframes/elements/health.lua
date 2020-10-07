7debe7c5c8a0b6b5034c37be2ed416a77c25df86
Chiel Kunkels <chiel@kunkels.me>
2020-10-08 00:57:43 +0200

    health: update coloring

--- unitframes/elements/health.lua
+++ unitframes/elements/health.lua
@@ -1,25 +1,14 @@
 local addon = select(2, ...)
 
 local function UpdateHealthColor(health, unit, cur, max)
-	local r, g, b, t
-	if health.disconnected and health.colorDisconnected or UnitIsDeadOrGhost(unit) then
-		health:SetValue(max)
-		t = colors.disconnected
-	elseif health.colorTapping and not UnitPlayerControlled(unit) and UnitIsTapDenied(unit) then
-		t = colors.tapped
-	else
-		t = {.15, .15, .15}
+	if health.disconnected or UnitIsDeadOrGhost(unit) then
+		health:SetValue(0)
 	end
 
-	if t then
-		r, g, b = t[1], t[2], t[3]
-	end
-
-	if b then
-		health:SetStatusBarColor(r, g, b)
-
-		local _, class = UnitClass(unit)
-		health.bd:SetBackdropColor(unpack(addon.colors.class[class]))
+	local color = addon.utils.UnitColor(unit)
+	if color then
+		health:SetStatusBarColor(.15, .15, .15)
+		health.bd:SetBackdropColor(unpack(color))
 	end
 end
 
