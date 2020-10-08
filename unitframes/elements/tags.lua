1a3652ec3ab5d6468afb7423c22c679928b9369b
Chiel Kunkels <chiel@kunkels.me>
2020-10-08 02:52:09 +0200

    raid: add basic raid frames

--- unitframes/elements/tags.lua
+++ unitframes/elements/tags.lua
@@ -16,3 +16,17 @@ oUF.Tags.Events['AelUI:power'] = 'UNIT_DISPLAYPOWER UNIT_MAXPOWER UNIT_POWER_FRE
 oUF.Tags.Methods['AelUI:power'] = function(unit)
 	return UnitPower(unit)
 end
+
+oUF.Tags.Events['AelUI:raidtext'] = 'UNIT_NAME_UPDATE UNIT_HEALTH UNIT_CONNECTION'
+oUF.Tags.Methods['AelUI:raidtext'] = function(unit)
+	local name = UnitName(unit)
+	local color = addon.utils.UnitColor(unit)
+
+	local offset = 3
+
+	if color then
+		return addon.utils.Hex(color)..name:utf8sub(0, offset)
+	end
+
+	return name:utf8sub(0, offset)
+end
---
+++ unitframes/units/raid.lua
@@ -0,0 +1,73 @@
+local addon = select(2, ...)
+
+local function UpdatePower(self, event, unit)
+	if(unit ~= self.unit) then return end
+
+	local visible = addon.utils.UnitIsHealer(unit)
+
+	if visible then
+		self.Power:SetMinMaxValues(0, UnitPowerMax(unit, Enum.PowerType.Mana))
+		self.Power:SetValue(UnitPower(unit, Enum.PowerType.Mana))
+	end
+
+	self.Health:SetPoint('BOTTOM', 0, visible and 5 or 0)
+	self.Power:SetShown(visible)
+end
+
+addon.units.raid = {
+	spawn = function(self)
+		local y = ((56 + 8) * 5)
+		local raid = {}
+		for i = 1, NUM_RAID_GROUPS do
+			raid[i] = self:SpawnHeader(
+				nil, nil, 'raid',
+				'showSolo', true,
+				'showParty', true,
+				'showPlayer', true,
+				'showRaid', true,
+				'maxColumns', 1,
+				'unitsPerColumn', 5,
+				'yOffset', -8,
+				'groupFilter', i,
+				'oUF-initialConfigFunction', [[
+					self:SetWidth(68)
+					self:SetHeight(56)
+				]]
+			)
+
+			if (i == 1) then
+				raid[i]:SetPoint('TOPLEFT', UIParent, 'BOTTOMLEFT', 620, y)
+			else
+				raid[i]:SetPoint('TOPLEFT', raid[i - 1], 'TOPRIGHT', 8, 0)
+			end
+		end
+	end,
+
+	style = function(self, unit)
+		self:RegisterForClicks('AnyUp')
+		self:SetScript('OnEnter', UnitFrame_OnEnter)
+		self:SetScript('OnLeave', UnitFrame_OnLeave)
+
+		self.colors = addon.colors
+
+		addon.elements.Health(self, unit)
+		self.Health:SetAllPoints()
+		self.Health:SetPoint('BOTTOM', 0, 5)
+
+		addon.elements.Power(self, unit)
+		self.Power:SetPoint('BOTTOMLEFT')
+		self.Power:SetPoint('BOTTOMRIGHT')
+		self.Power:SetHeight(4)
+		self.Power:SetStatusBarColor(unpack(self.colors.power.MANA))
+		self.Power.Override = UpdatePower
+
+		local text = addon.elements.Text(self.Health, { size = 16 })
+		self:Tag(text, '[AelUI:raidtext]')
+		text:SetPoint('BOTTOM', self, 'BOTTOM', 0, 12)
+
+		self.Range = {
+			insideAlpha = 1,
+			outsideAlpha = .6,
+		}
+	end,
+}
